9502d8848a8dd1c0e320bc8f9bdf1b62515622b1
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-09-11 21:54:09 -0500

    Testing bypass

--- main.go
+++ main.go
@@ -4,70 +4,105 @@ import (
 	"Monitor/browser"
 	"Monitor/cfclient"
 	"Monitor/constant"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
+	"Monitor/models"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+
+	// "io/ioutil"
 	"log"
-	"math/big"
-	"net/http"
-	"net/url"
+	// "math/big"
+	// "net/http"
+	// "net/url"
 	"time"
 
+	http "github.com/saucesteals/fhttp"
+	"github.com/saucesteals/mimic"
 )
 
 // Language: go
+var latestVersion = mimic.MustGetLatestVersion(mimic.PlatformWindows)
+var m, _ = mimic.Chromium(mimic.BrandChrome, latestVersion)
 
 func request() {
 	start := time.Now()
+	// Create a new client
+	var client *http.Client
+	// Create a new request
+	var req *http.Request
 
-	
-	// ConfigureClient(client, constant.URL, constant.AGENT)
-	req, err := http.NewRequest("GET", constant.URL, nil)
-	if err != nil {
-		log.Fatal("Request cannot be sent.", err.Error())
-	}
-
-	transport, err := createTransport(constant.PROXY)
-	if err != nil {
-		log.Fatal("Proxy cannot be created.", err.Error())
-	}
-	client.Transport = transport
-
-	resp, err := client.Do(req)
+	client , err := initClient(constant.PROXY)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(body))
+
+
+
+	// req, err := http.NewRequest("GET", constant.URL, nil)
+	// if err != nil {
+	// 	log.Fatal("Request cannot be sent.", err.Error())
+	// }
+
+	// transport, err := createTransport(constant.PROXY)
+	// if err != nil {
+	// 	log.Fatal("Proxy cannot be created.", err.Error())
+	// }
+	// client.Transport = transport
+
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(string(body))
 	fmt.Printf("<|%v|> [%s]\n", resp.Status, time.Since(start))
 
 }
 
 
-func createTransport(proxy string) (*http.Transport, error) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
+func initClient(proxy string) (*http.Client, error) {
+	transport, err := createTransport(proxy)
+	if err != nil {
+		return nil, err
 	}
-	if proxy != "" {
-		proxyURL, err := url.Parse(proxy)
+
+	return &http.Client{
+		Transport: m.ConfigureTransport(transport),
+	}, nil
+}
+
+
+
+func createTransport(proxy string) (*http.Transport, error) {
+	if len(proxy) != 0 {
+		proxyUrl, err := url.Parse(proxy)
 		if err != nil {
 			return nil, err
 		}
-		transport.Proxy = http.ProxyURL(proxyURL)
+		return &http.Transport{Proxy: http.ProxyURL(proxyUrl)}, nil
+	} else {
+		return &http.Transport{}, nil
 	}
-	return transport, nil
 }
+
+// func createTransport(proxy string) (*http.Transport, error) {
+// 	transport := &http.Transport{
+// 		TLSClientConfig: &tls.Config{
+// 			InsecureSkipVerify: true,
+// 		},
+// 	}
+// 	if proxy != "" {
+// 		proxyURL, err := url.Parse(proxy)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		transport.Proxy = http.ProxyURL(proxyURL)
+// 	}
+// 	return transport, nil
+// }
 
 func ConfigureClient(client *http.Client, target string, agent string) error {
 	// Initialize the client with the things we need to bypass cloudflare
